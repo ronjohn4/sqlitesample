a64414371ce1139b4eedcfdab10ca157c5bbfc1d
Ron <ronjohn4@gmail.com>
2018-09-10 18:21:19 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,122 @@
+# Ron Johnson
+# 9/8/2018
+#
+# Sample of DB operations typically needed against sqlite3.
+# Most of this code came from somewhere else, see urls below.
+#
+# http://www.sqlitetutorial.net
+
+import sqlite3
+
+# database = "C:\\sqlite\db\pythonsqlite.db"
+database = "pythonsqlite.db"
+
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file.  The database will be created if it doesn't exist.
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except sqlite3.Error as e:
+        print(e)
+
+    return None
+
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except sqlite3.Error as e:
+        print(e)
+
+
+def create_tables(database):
+
+    sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
+                                            id integer PRIMARY KEY,
+                                            name text NOT NULL,
+                                            begin_date text,
+                                            end_date text
+                                        ); """
+
+    sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
+                                        id integer PRIMARY KEY,
+                                        name text NOT NULL,
+                                        priority integer,
+                                        status_id integer NOT NULL,
+                                        project_id integer NOT NULL,
+                                        begin_date text NOT NULL,
+                                        end_date text NOT NULL,
+                                        FOREIGN KEY (project_id) REFERENCES projects (id)
+                                    );"""
+
+    # create a database connection
+    conn = create_connection(database)
+    if conn is not None:
+        # create projects table
+        create_table(conn, sql_create_projects_table)
+        # create tasks table
+        create_table(conn, sql_create_tasks_table)
+    else:
+        print("Error! cannot create the database connection.")
+
+
+def create_project(conn, project):
+    """
+    Create a new project into the projects table
+    :param conn:
+    :param project:
+    :return: project id
+    """
+    sql = ''' INSERT INTO projects(name,begin_date,end_date)
+              VALUES(?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, project)
+    return cur.lastrowid
+
+
+def create_task(conn, task):
+    """
+    Create a new task
+    :param conn:
+    :param task:
+    :return:
+    """
+
+    sql = ''' INSERT INTO tasks(name,priority,status_id,project_id,begin_date,end_date)
+              VALUES(?,?,?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, task)
+    return cur.lastrowid
+
+
+if __name__ == "__main__":
+    create_tables(database)
+
+    conn = create_connection(database)
+    with conn:
+        # create a new project
+        project = ('Cool App with SQLite & Python', '2015-01-01', '2015-01-30');
+        project_id = create_project(conn, project)
+
+        # tasks
+        task_1 = ('Analyze the requirements of the app', 1, 1, project_id, '2015-01-01', '2015-01-02')
+        task_2 = ('Confirm with user about the top requirements', 1, 1, project_id, '2015-01-03', '2015-01-05')
+
+        # create tasks
+        create_task(conn, task_1)
+        create_task(conn, task_2)
+
+
+
